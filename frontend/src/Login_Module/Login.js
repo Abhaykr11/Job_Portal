ec1752790fcaebad9c4a1de64124908df0af309c
Abhay <abhaykrbhat@gmail.com>
2025-02-08 02:30:10 +0530

    2:26 am

--- frontend/src/Login_Module/Login.js
+++ frontend/src/Login_Module/Login.js
@@ -112,7 +112,7 @@ function Login() {
             </div>
           </div>
           <div className="flex justify-end">
-            <Link to="/register">
+            <Link to="register">
               <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
                 Register
               </button>
